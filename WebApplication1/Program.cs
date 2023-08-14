33fbd92fc4d7a00e018b3623edeb4796fb940107
Ricardo <ricardoserafim1920@gmail.com>
2023-08-14 17:04:30 -0300

    Correção de Namespace.

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 
-namespace WebApplication1
+namespace CleanArchMvc.API
 {
     public class Program
     {
--- WebApplication1/Startup.cs
+++ WebApplication1/Startup.cs
@@ -5,7 +5,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-namespace WebApplication1
+namespace CleanArchMvc.API
 {
     public class Startup
     {
